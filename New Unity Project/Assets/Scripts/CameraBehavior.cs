8da1a5d9b3e5db19f4d41060aec073c8304cfbdc
Leo Velosa <64762864+LeoVelosa@users.noreply.github.com>
2021-02-18 21:51:18 -0500

    Update

---
+++ New Unity Project/Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour
+{
+    public Vector3 camOffset = new Vector3(0, 1.2f, -2.6f);
+
+    private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("TurtleRed").transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        this.transform.position = target.TransformPoint(camOffset);
+
+        this.transform.LookAt(target,Vector3.up);
+    }
+}
---
+++ New Unity Project/Assets/Scripts/FishBehavior.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FishBehavior : MonoBehaviour
+{
+    public Transform turtle;
+    
+    public Transform patrolRoute;
+
+    public List<Transform> locations;
+
+    private int locationIndex = 0;
+
+    private NavMeshAgent fish;
+
+    void Start()
+    {
+        fish = GetComponent<NavMeshAgent>();
+
+        turtle = GameObject.Find("TurtleRed").transform;
+
+        InitializePatrolRoute();
+
+        MoveToNextPatrolLocation();
+    }
+
+    void Update()
+    {
+        if(fish.remainingDistance < 0.2f && !fish.pathPending)
+        {
+            MoveToNextPatrolLocation();
+        }
+    }
+
+    void InitializePatrolRoute()
+    {
+        foreach(Transform child in patrolRoute)
+        {
+            locations.Add(child);
+        }
+    }
+
+    void MoveToNextPatrolLocation()
+    {
+        if(locations.Count == 0)
+        {
+            return;
+        }
+
+        fish.destination = locations[locationIndex].position;
+
+        locationIndex = (locationIndex + 1) % locations.Count;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "TurtleRed")
+        {
+            fish.destination = turtle.position;
+
+            Debug.Log("Turtle incoming!!! Defend the food!!!");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.name == "TurtleRed")
+        {
+            Debug.Log("Turtle out of range, resume patrol");
+        }
+    }
+}
